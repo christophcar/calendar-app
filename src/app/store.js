b7d1569180cf8685d8a7a34145da482f416aba4d
Christoph <carstens.christoph@gmail.com>
2019-04-03 07:47:28 +0200

    Add store logic

--- src/app/store.js
+++ src/app/store.js
@@ -4,5 +4,15 @@ import { seedData } from './seed.js'
 export const store = {
   state: {
     seedData
+  },
+  getActiveDay() {
+    return this.state.seedData.find(day => day.active)
+  },
+  // Die map() (engl. abbilden) Methode wendet auf jedes Element des Arrays die
+  // bereitgestellte Funktion an und gibt das Ergebnis in einem neuen Array zurück.
+  setActiveDay(dayId) {
+    this.state.seedData.map(dayObj => {
+      dayObj.id === dayId ? (dayObj.active = true) : (dayObj.active = false)
+    })
   }
 }
