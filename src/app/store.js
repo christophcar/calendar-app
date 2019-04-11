e11aea575e1713af19217c217799ede88e825210
Christoph <carstens.christoph@gmail.com>
2019-04-11 06:03:11 +0200

    Refactoring of store

--- src/app/store.js
+++ src/app/store.js
@@ -5,30 +5,34 @@ export const store = {
   state: {
     seedData
   },
-  // return the day object that has active: true
+
   getActiveDay() {
     return this.state.seedData.find(day => day.active)
   },
-  // set the selected day to active: true and all other days to active: false
+
   setActiveDay(dayId) {
     this.state.seedData.map(dayObj => {
       dayObj.id === dayId ? (dayObj.active = true) : (dayObj.active = false)
     })
   },
+
   submitEvent(eventDetails) {
     const activeDay = this.getActiveDay()
     activeDay.events.push({ details: eventDetails, edit: false })
   },
+
   editEvent(dayId, eventDetails) {
     this.resetEditOfAllEvents() // reset thruthy edit events to false
     const eventObj = this.getEventObject(dayId, eventDetails)
     eventObj.edit = true
   },
+
   updateEvent(dayId, originalEventDetails, newEventDetails) {
     const eventObj = this.getEventObject(dayId, originalEventDetails)
     eventObj.details = newEventDetails
     eventObj.edit = false
   },
+
   deleteEvent(dayId, eventDetails) {
     const dayObj = this.state.seedData.find(day => day.id === dayId)
     const eventIndexToRemove = dayObj.events.indexOf(event => event.details === eventDetails)
@@ -40,6 +44,7 @@ export const store = {
     const dayObj = this.state.seedData.find(day => day.id === dayId)
     return dayObj.events.find(event => event.details === eventDetails)
   },
+
   // helper function that runs through all data and sets edit prop to false
   resetEditOfAllEvents() {
     this.state.seedData.map(dayObj => {
